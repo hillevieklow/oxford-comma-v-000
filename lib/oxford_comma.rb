ffc437c5a0b6178cea14f3eb70eee3335afe40fe
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-31 17:43:02 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
+  if array.size == 1
+    array
+  elsif array.size == 2
+    array.join(" and ")
+
+
   array.join(", ")
   if array.size > 2
     last_element = "and #{array[-1]}"
